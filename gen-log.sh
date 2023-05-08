eca15261a7bd1917c82e08fe8d6d1382ff6f1ea0
Alan <adong@users.noreply.github.com>
2023-05-08 09:58:05 -0700

    Update gen-log.sh

--- gen-log.sh
+++ gen-log.sh
@@ -41,3 +41,16 @@ do
   printf '%.500s\n' "$part"'|'"$hash"
   count=$((count + 1))
 done
+
+
+while true
+do
+    random_ip=$(dd if=/dev/urandom bs=4 count=1 2>/dev/null | od -An -tu1 | sed -e 's/^ *//' -e 's/  */./g')
+    random_size=$(( (RANDOM % 65535) + 1 ))
+
+    current_date_time=$(date '+%d/%b/%Y:%H:%M:%S %z')
+
+    echo "$random_ip - - [$current_date_time] \"GET /data.php HTTP/1.1\" 200 $random_size" | tee -a 'random_log'
+
+    sleep $[ ( $RANDOM % 10 )  + 1 ]s
+done
